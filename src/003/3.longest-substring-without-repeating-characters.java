395da1aff4554a8269d1790da06b88ac082f7a1e
chenxiaokang <chenxiaokang97@163.com>
2019-02-12 14:43:55 +0800

    [add] add 003

--- src/003/3.longest-substring-without-repeating-characters.java
+++ src/003/3.longest-substring-without-repeating-characters.java
@@ -1,5 +1,16 @@
 class Solution {
     public int lengthOfLongestSubstring(String s) {
-        
+        int[] prev = new int[128];
+        int left = 0,max = 0;
+        for(int i = 0;i < s.length(); ++i){
+            if(prev[s.charAt(i)] > left){
+                left = prev[s.charAt(i)];
+            }
+            prev[s.charAt(i)] = i + 1;
+            if(max <= i - left + 1){
+                max = i - left + 1;
+            }
+        }
+        return max;
     }
 }
