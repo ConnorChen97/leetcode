4995dd261b23d82ec157987ccbadee75fa4e6a35
ConnorChen <chenxiaokang97@163.com>
2019-08-19 22:03:56 +0800

    [fix] format

--- src/001/1.two-sum.js
+++ src/001/1.two-sum.js
@@ -3,11 +3,11 @@
  * @param {number} target
  * @return {number[]}
  */
-var twoSum = function(nums, target) {
+const twoSum = (nums, target) => {
     const map = new Map();
     for(let i = 0;i < nums.length;i ++){
         const complement = target - nums[i];
-        if(map.has(complement)) return [map.get(complement), i]
+        if(map.has(complement)) return [map.get(complement), i];
         map.set(nums[i], i);
     }
 };
