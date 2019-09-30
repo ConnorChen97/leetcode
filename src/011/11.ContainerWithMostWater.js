24b3c78c71cce672ce86a24b1dc1fd106d49a2c3
Connor Chen <chenxiaokang97@163.com>
2019-09-30 10:35:16 +0800

    [add] add ContainerWithMostWater

--- src/011/11.ContainerWithMostWater.js
+++ src/011/11.ContainerWithMostWater.js
@@ -7,3 +7,16 @@ const search = (height) => {
     }
     return max;
 }
+
+const search2 = (height) => {
+    let i = 0;
+    let j = height.length - 1;
+    let max = 0;
+    while (i < j) {
+        max = Math.max(max, Math.abs(Math.abs(i - j) * Math.min(height[i], height[j])));
+        height[i] < height[j] ? i++ : j--;
+    }
+    return max;
+}
+
+console.log(search([1, 1]));
