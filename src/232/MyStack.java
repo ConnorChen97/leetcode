c0b388fcb562f3520ca5316cf71468faf6a625d6
chenxiaokang <chenxiaokang97@163.com>
2020-08-05 17:41:05 +0800

    add: add 71 and 232

---
+++ src/232/MyStack.java
@@ -0,0 +1,72 @@
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.Stack;
+import java.util.concurrent.DelayQueue;
+
+/*
+ * @lc app=leetcode.cn id=232 lang=java
+ *
+ * [232] 用栈实现队列
+ */
+
+// @lc code=start
+class MyQueue {
+
+    Stack<Integer> stack1;
+    Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack();
+        stack2 = new Stack();
+    }
+    
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+    
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        int length = stack1.size();
+        for (int i = 0;i < length; i++) {
+            stack2.push(stack1.pop());
+        }
+        int temp = stack2.pop();
+        for (int i = 0;i < length - 1; i++) {
+            stack1.push(stack2.pop());
+        }
+        return temp;
+    }
+    
+    /** Get the front element. */
+    public int peek() {
+        int length = stack1.size();
+        for (int i = 0;i < length; i++) {
+            stack2.push(stack1.pop());
+        }
+        int temp = stack2.peek();
+        for (int i = 0;i < length; i++) {
+            stack1.push(stack2.pop());
+        }
+        return temp;
+    }
+    
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
+// @lc code=end
+
---
+++ src/71/Solution.java
@@ -0,0 +1,38 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+/*
+ * @lc app=leetcode.cn id=71 lang=java
+ *
+ * [71] 简化路径
+ */
+
+// @lc code=start
+class Solution {
+    public String simplifyPath(String path) {
+        String[] paths = path.split("/");
+        Deque<String> stack = new LinkedList();
+        for (String p : paths) {
+            if ("..".equals(p) ) {
+                if (!stack.isEmpty()) {
+                    stack.pop();
+                }
+                continue;
+            } else if (!".".equals(p) && !"".equals(p)) {
+                stack.push(p);
+            }
+        }
+        String result = "";
+        int length = stack.size();
+        for (int i = 0; i < length; i++) {
+            result += "/" + stack.removeLast();
+        }
+        if ("".equals(result)) {
+            result = "/";
+        }
+        return result;
+    }
+
+}
+// @lc code=end
+
